e741b7e22354a4d3b74903a07d6804c1cb8a685a
saisasank001 <saisasank001@gmail.com>
2017-02-06 14:07:48 -0500

    clokam/java-cd-app#8

--- src/main/java/wasdev/sample/servlet/SimpleServlet.java
+++ src/main/java/wasdev/sample/servlet/SimpleServlet.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import wasdev.sample.functions.*;
 
-/**
+/***
  * Servlet implementation class SimpleServlet..
   Servlet implementation class SimpleServlet
   New comment has been added.
